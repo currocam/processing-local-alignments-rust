3c6b96df29c935304d06c57d85b8672720da86af
currocam <campuzanocurro@gmail.com>
2022-09-19 10:46:56 +0200

    feat: standar input read

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
+use std::io;
+use std::io::prelude::*;
 use fancy_regex::Regex;
-use std;
+use clap::Parser;
 
-fn main() {
-    split_pairs("Hello, world!");
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    mode: String,
+    // #[clap(parse(from_os_str))]
+    // path: std::path::PathBuf,
+}
+fn main() ->io::Result<()>{
+    let args = Cli::parse();
+    let stdin = io::stdin();
+    match args.mode.as_str(){
+        "to_cig" => {
+            let mut lines = stdin.lock().lines();
+            while let Some(Ok(seq1)) = lines.next(){
+                let unaligned1 = &seq1.chars().filter(|x| *x != '-').collect::<String>();
+                print!("{}\t", unaligned1);
+                let seq2 = &lines.next().unwrap().unwrap();
+                let unaligned2 = &seq2.chars().filter(|x| *x != '-').collect::<String>();
+                print!("{}\t", unaligned2);
+                println!("{}", edits_to_cigar(
+                    &edits(&seq1, &seq2)));
+                lines.next();
+            }              
+                io::stdout().flush().unwrap();
+        },
+        "from_cig" => {
+            for line in stdin.lock().lines() {
+                let line = line.unwrap();
+                let mut splits = line.split("\t");
+                let (seq1, seq2) = align(
+                     splits.next().unwrap(),
+                     splits.next().unwrap(),
+                     &cigar_to_edits(splits.next().unwrap())
+                );
+                println!("{}\n{}", seq1, seq2);
+                io::stdout().flush().unwrap();
+
+            }
+        },
+        _ => ()
+    }
+    Ok(())
 }
 
 fn align(x:&str, y:&str, edits:&str) -> (String, String) {
